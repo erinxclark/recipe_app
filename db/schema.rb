86da628b1d243410678ee2718b72f4f38bcd137a
Erin Clark <erinxclark@gmail.com>
2015-10-27 13:40:45 -0700

    Tag and Recipe db migrated and accessible

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151027164156) do
+ActiveRecord::Schema.define(version: 20151027181450) do
 
   create_table "recipes", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20151027164156) do
     t.text     "instructions"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
